4401a51fad94286f1ae0b482a6a3f0573ad94ca7
Mark Barho <mark.barho@gmail.com>
2022-06-13 00:28:17 +0700

    quick db functionality

--- pkg/adapters/db.py
+++ pkg/adapters/db.py
@@ -1,6 +1,8 @@
 # TODO: fill database adapter
+# TODO: handle relations post-user on create-like-unlike-delete actions
 from fastapi_sqlalchemy import db
 from models.models import User as ModelUser
+from models.models import Post as ModelPost
 
 
 class UserDBAdapter:
@@ -10,12 +12,14 @@ class UserDBAdapter:
         return user
 
     @staticmethod
-    def get_user_by_id():
-        pass
+    def get_user_by_id(user_id: int):
+        user = db.session.query(ModelUser).filter_by(id=user_id).first()
+        return user
 
     @staticmethod
     def get_all_users():
-        pass
+        users = db.session.query(ModelUser).all()
+        return users
 
     @staticmethod
     def add_post_created():
@@ -32,11 +36,11 @@ class UserDBAdapter:
 
 class PostDBAdapter:
     @staticmethod
-    def like_post():
+    def like_post(user_id: int, post_id: int):
         pass
 
     @staticmethod
-    def unlike_post():
+    def unlike_post(user_id: int, post_id: int):
         pass
 
     @staticmethod
@@ -44,13 +48,16 @@ class PostDBAdapter:
         pass
 
     @staticmethod
-    def get_post_by_id():
-        pass
+    def get_post_by_id(post_id: int):
+        post = db.session.query(ModelPost).filter_by(id=post_id).first()
+        return post
 
     @staticmethod
     def get_all_posts():
-        pass
+        posts = db.session.query(ModelPost).all()
+        return posts
 
     @staticmethod
-    def get_post_by_user():
-        pass
+    def get_posts_by_user(user_id: int):
+        posts = db.session.query(ModelPost).filter_by(author_id=user_id)
+        return posts
--- pkg/todos.py
+++ pkg/todos.py
@@ -4,7 +4,8 @@
 # TODO: BOT!
 # TODO: add encryption, never sand bare pockets
 # TODO: move hard values to separate config/module
-# TODO: review controller types (get, post, put...) and assign propper when needed
+# TODO: review controller types (get, post, put...) and assign proper when needed
 # TODO: move sqlalchemy to ScopedSession to ensure thread safeness
-# TODO: create an sqlalchemy adapter for db operations (shoud i use context manager?:)
+# TODO: create an sqlalchemy adapter for db operations (should i use context manager?:)
 # TODO: store tokens in reddit i guess?
+# TODO: add exception handling(DB)
